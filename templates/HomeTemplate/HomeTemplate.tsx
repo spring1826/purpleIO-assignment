31fac7fd266aad25c4ef0c1fe538a2abb0ebb052
spring <dongwon1826@gmail.com>
2022-04-15 19:31:23 +0900

    feat: add hometemplate

---
+++ templates/HomeTemplate/HomeTemplate.tsx
@@ -0,0 +1,9 @@
+import * as S from "./HomeTemplate.style";
+
+interface HomeTemplateProps {}
+
+const HomeTemplate: React.FC<HomeTemplateProps> = (props) => {
+  return <S.Wrapper>HomeTemplate</S.Wrapper>;
+};
+
+export default HomeTemplate;
