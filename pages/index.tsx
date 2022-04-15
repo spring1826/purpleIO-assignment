e209e07e47432674ecc7f76b52e424c184093fd1
spring <dongwon1826@gmail.com>
2022-04-15 18:20:31 +0900

    chore: 환경 설정

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
